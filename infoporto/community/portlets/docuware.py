4a521c17355a1e2163db946f42487d97cf9eee11
root <root@debianportale.lsct.contship.net>
2016-02-22 09:10:53 +0100

    docuware

--- infoporto/community/portlets/docuware.py
+++ infoporto/community/portlets/docuware.py
@@ -41,7 +41,7 @@ class Renderer(base.Renderer):
         user = api.user.get_current()
         username = user.getMemberId()
 
-        hashed = hashlib.md5("%s_%s" % (username, SECRET)).hexdigest()
+        hashed = hashlib.md5("%s%s" % (username, SECRET)).hexdigest()
 
         return HOST % (username, hashed)
 
